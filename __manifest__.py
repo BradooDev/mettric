c4ca84061d06165161eb9e642bf545ab32c5ddc4
Virgílio Santos <virgilio.santos@gmail.com>
2017-08-28 17:50:22 -0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "mettric",
+
+    'summary': """
+        Módulo que carrega os módulos básicos e dados básicos da Mettric
+        """,
+
+    'description': """
+        Módulo que carrega os módulos base: Contabilidade, NFse, Localização Brasileira, Boleto e CNAB
+    """,
+
+    'author': "Virgílio Santos, BradooTech",
+    'website': "http://www.bradootech.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Specific Industry Applications',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account', 'br_cnab', 'br_zip', 'br_account_einvoice',
+                'br_data_account', 'br_base', 'br_account', 'br_nfe',
+                'br_nfse', 'br_boleto', 'sync_numero_nfse_bradoo',
+                'coa_bradoo', 'ir_csll_bradoo', 'reemissao_nfs_bradoo'
+
+                ],
+
+    # always loaded
+    'data': [
+        'data/res_company.xml',
+        'data/br_chart_data.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+    ],
+    'post_init_hook': 'execute_default',
+}
